b8a00a03c50f4a80833d2f25bb9841f1c04a6011
Serhii Samborskyi <ss@develup.pro>
2016-07-12 17:04:24 +0300

    Fix XPath expressions

--- Logic/Poster.cs
+++ Logic/Poster.cs
@@ -8,15 +8,15 @@ namespace Logic
 {
     public class Poster
     {
-            public string DataLink { get; set; }
-            public string ImageLink { get; set; }
+        public string DataLink { get; set; }
+        public string ImageLink { get; set; }
 
-            public string Title { get; set; }
+        public string Title { get; set; }
 
-            public string Description { get; set; }
+        public string Description { get; set; }
 
-            public string Rating { get; set; }
-            public string Year { get; set; }
+        public string Rating { get; set; }
+        public string Year { get; set; }
         //public string Genres { get; set; }
         //public string Contries { get; set; }
         //public string Length { get; set; }
